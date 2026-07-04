2a358b6ee308fc19ece6dd8c8995c2086c252069
Suryabhan-Patel <psuryabhan017@gmail.com>
2026-07-04 20:53:40 +0530

    GCD

---
+++ code232.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class code232{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int first=a;
+        int second=b;
+
+
+        while(b!=0){
+            int temp =b;
+            b = a%b;
+            a=temp;
+        }
+        System.out.println("GCD OF THE PAIRS IS "+a);
+        System.out.println("LCM OF PAIR IS "+(first*second)/a);
+    }
+}
