b9fa6586fc1a7b852b3bd3584b30e86941b922a5
Suryabhan-Patel <psuryabhan017@gmail.com>
2026-07-16 22:59:56 +0530

    longest substring without repeating chracters

---
+++ code252.java
@@ -0,0 +1,20 @@
+class code252 {
+    public double findMaxAverage(int[] nums, int k) {
+        int maxi= Integer.MIN_VALUE;
+        int low=0;
+        int high=k-1;
+        int sum=0;
+        for(int i = 0 ;i <=high;i++){
+            sum=sum+nums[i];
+        }
+        maxi=Math.max(maxi,sum);
+        for(high=high+1; high<nums.length; high++){
+            sum=sum-nums[low];
+            low++;
+            sum=sum+nums[high];
+            maxi=Math.max(maxi,sum);
+        }
+        return (double)maxi/k;
+        
+    }
+}
