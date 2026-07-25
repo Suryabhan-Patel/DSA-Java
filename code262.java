081a65af25fb48ec63a7c28465cf79d28904e6da
Suryabhan-Patel <psuryabhan017@gmail.com>
2026-07-25 14:42:43 +0530

    power of 2 no

---
+++ code262.java
@@ -0,0 +1,11 @@
+public class code262 {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) return false;
+
+        long x = 1;
+        while (x < n) {
+            x *= 2;
+        }
+        return x == n;
+    }
+}
